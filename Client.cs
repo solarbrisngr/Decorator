9daaab208cbc2e373ab873efb7b3b71808a10168
solarbrisngr <59808313+solarbrisngr@users.noreply.github.com>
2020-02-26 13:59:40 -0700

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown("b"))
+        {
+            IRifle rifle = new BasicRifle();
+            Debug.Log("Basic accuracy: " + rifle.GetAccuracy());
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            IRifle rifle = new BasicRifle();
+            rifle = new WithScope(rifle);
+            Debug.Log("Scope Accuracy: " + rifle.GetAccuracy());
+        }
+
+        if (Input.GetKeyDown("t"))
+        {
+            IRifle rifle = new BasicRifle();
+            rifle = new WithScope(new WithStabilizer(rifle));
+            Debug.Log("Stabilizer + Scope Accuracy: " + rifle.GetAccuracy());
+        }
+
+        if (Input.GetKeyDown("l"))
+        {
+            IRifle rifle = new BasicRifle();
+            rifle = new WithLasers(rifle);
+            Debug.Log("Laser: " + rifle.GetAccuracy());
+        }
+
+        if (Input.GetKeyDown("u"))
+        {
+            IRifle rifle = new BasicRifle();
+            rifle = new WithBayonette(rifle);
+            Debug.Log("Bayonette: " + rifle.GetAccuracy());
+        }
+    }
+}
---
+++ RifleDecorator.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class RifleDecorator : IRifle
+{
+    protected IRifle m_DecoratedRifle;
+
+    //constructor
+    public RifleDecorator(IRifle rifle)
+    {
+        m_DecoratedRifle = rifle;
+    }
+
+    public virtual float GetAccuracy()
+    {
+        return m_DecoratedRifle.GetAccuracy();
+    }
+}
+
+public class WithScope : RifleDecorator, IRifle
+{
+    private float m_ScopeAccuracy = 20.0f;
+
+    //Constructor
+    public WithScope(IRifle rifle) : base(rifle) { }
+
+    public override float GetAccuracy()
+    {
+        return base.GetAccuracy() + m_ScopeAccuracy;
+    }
+}
+
+public class WithStabilizer : RifleDecorator, IRifle
+{
+    private float m_StabilizerAccuracy = 10.0f;
+
+    public WithStabilizer(IRifle rifle) : base(rifle) { }
+
+    public override float GetAccuracy()
+    {
+        return base.GetAccuracy() + m_StabilizerAccuracy;
+    }
+}
+
+public class WithLasers : RifleDecorator, IRifle
+{
+    private float m_LaserAccuracy = 7.0f;
+
+    public WithLasers(IRifle rifle) : base(rifle) { }
+
+    public override float GetAccuracy()
+    {
+        return base.GetAccuracy() + m_LaserAccuracy;
+    }
+}
+
+public class WithBayonette : RifleDecorator, IRifle
+{
+    private float m_BayonetteAccuracy = 1.0f;
+
+    public WithBayonette(IRifle rifle) : base(rifle) { }
+
+    public override float GetAccuracy()
+    {
+        return base.GetAccuracy() + m_BayonetteAccuracy;
+    }
+}
